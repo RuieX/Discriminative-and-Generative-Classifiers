f01512d19d4b286aa6a40a486e1ea444ae70260f
RuieX <runjiexia28@gmail.com>
2022-12-25 01:03:31 +0100

    applying eva to all notebooks

--- src/utilities/evaluation.py
+++ src/utilities/evaluation.py
@@ -3,16 +3,15 @@ from __future__ import annotations
 import os
 import joblib
 import pandas as pd
-import statistics as stat
 
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 
 
 class Evaluation:
-    def __init__(self, y_real: pd.DataFrame, y_pred: pd.DataFrame):
+    def __init__(self, y_true: pd.DataFrame, y_pred: pd.DataFrame):
         self.y_pred = y_pred
-        self.y_real = y_real
-        self.acc_score = accuracy_score(y_real, y_pred)
+        self.y_real = y_true
+        self.acc_score = accuracy_score(y_true, y_pred)
 
     def acc_eval(self):
         """
@@ -21,40 +20,34 @@ class Evaluation:
             otherwise it raises an exception
         :return: accuracy score of prediction
         """
-        print("--------------Model Evaluations:--------------")
+        print("-----Model Evaluations:-----")
         print('Accuracy score: {}'.format(self.acc_score))
-
-    # TODO da appunti
-    # from sklearn.metrics import plot_confusion_matrix
-    # plot_confusion_matrix(estimator=dt, X=X_test_enc, y_true=y_test, cmap = 'Blues_r'):
-    # è deprecato,
-    # Function `plot_confusion_matrix` is deprecated in 1.0 and will be removed in 1.2.
-    # Use one of the class methods: ConfusionMatrixDisplay.from_predictions or ConfusionMatrixDisplay.from_estimator.
     
     def conf_mat(self):
-        cm = confusion_matrix(y_true=self.y_real, y_pred=self.y_pred)
+        cmat = confusion_matrix(y_true=self.y_real, y_pred=self.y_pred)
         disp = ConfusionMatrixDisplay(
-            confusion_matrix=cm,
+            confusion_matrix=cmat,
             display_labels=[str(n) for n in range(10)]
         )
         disp.plot(cmap='Blues_r')
 
+# TODO explain what's the confusion matrix
+# TODO explain why i use accuracy and not something like F1 score
+
 
 class EvaluatedModel:
-    def __init__(self, model, train_eval: Evaluation, test_eval: Evaluation):
+    def __init__(self, model, name, train_eval: Evaluation, test_eval: Evaluation):
         self.model = model
+        self.model_name = name
         self.train_eval = train_eval
         self.test_eval = test_eval
 
-    def save_evaluation(self, model):
+    def save_evaluation(self):
         if not os.path.exists('../models_evaluation'):
             os.mkdir('../models_evaluation')
 
-        joblib.dump(self, f'../models_evaluation/{model}.pkl')
+        joblib.dump(self, f'../models_evaluation/{self.model_name}.pkl')
 
     @staticmethod
-    def load_evaluation(model):
-        return joblib.load(f'../models_evaluation/{model}.pkl')
-
-# TODO explain what's the confusion matrix
-# TODO explain why i use accuracy and not something like F1 score
+    def load_evaluation(model_name):
+        return joblib.load(f'../models_evaluation/{model_name}.pkl')
