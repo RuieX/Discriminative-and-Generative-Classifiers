0cecedfffd21b6b7be69a183b61dfa9888e33c69
RuieX <runjiexia28@gmail.com>
2022-12-26 12:50:09 +0100

    .

--- src/classifiers/knn.py
+++ src/classifiers/knn.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
-
 import numpy as np
 import pandas as pd
 import scipy.stats as st
@@ -10,73 +8,56 @@ from sklearn.metrics.pairwise import euclidean_distances, manhattan_distances
 from sklearn.utils.validation import check_X_y
 
 
-class BaseClassifier(BaseEstimator, ClassifierMixin, ABC):
-    @abstractmethod
-    def fit(self, X: np.ndarray, y: np.ndarray) -> BaseClassifier:
-        pass
-
-    @abstractmethod
-    def predict(self, X: np.ndarray) -> BaseClassifier:
-        pass
-
-
-class KNN(BaseClassifier):
-    # X_train: np.ndarray = None
-    # """
-    # Training samples.
-    # """
-    #
-    # y_train: np.ndarray = None
-    # """
-    # Training labels.
-    # """
-    #
-    # k: int
-    # """
-    # Number of neighbors to consider.
-    # """
-    #
-    # metric: str
-    # """
-    # Metric for distance computation.
-    # Default is “euclidean”, otherwise "manhattan".
-    # """
-
+class KNN(BaseEstimator, ClassifierMixin):
     def __init__(self, k: int = 5, metric: str = "euclidean"):
+        """
+
+        :param k: Number of neighbors to consider.
+        :param metric: Metric for distance computation. Default is “euclidean”, otherwise "manhattan".
+        """
         self.k = k
         self.metric = metric
+        self.X_train = None
+        self.y_train = None
 
     def fit(self, X_data: pd.DataFrame, y_data: pd.DataFrame | np.ndarray) -> KNN:
-        # Check that X and y have consistent shapes
+        """
+        Checks X and y for consistent length, and convert them to ndarray if necessary
+        the shape of X is expected to be (n_samples, n_features)
+        the shape of y is expected to be (n_samples, 1)
+        :param X_data: Training samples.
+        :param y_data: Training labels.
+        :return:
+        """
         X_data, y_data = check_X_y(X_data, y_data)
         self.X_train = X_data
         self.y_train = y_data
 
         return self
 
-    # def predict(self, X_test: np.ndarray) -> np.ndarray:
-    #     output_pred = []
-    #
-    #     # Calculating distances
-    #     for i in range(X_test.shape[0]):  # for each sample in X_test
-    #         distances = []  # To store the distances
-    #         for j in range(self.X_train.shape[0]):  # for each sample in X_train
-    #             if self.metric == "euclidean":
-    #                 dist = euclidean_distances(self.X_train[j, :], X_test[i, :])  # Euclidean distance
-    #             else:
-    #                 dist = manhattan_distances(self.X_train[j, :], X_test[i, :])  # Manhattan distance
-    #             distances.append((dist, self.y_train[j]))
-    #         distances = sorted(distances)  # sorting distances in ascending order, # Order each neighbor
-    #
-    #         # Getting k nearest neighbors
-    #         neighbors = []
-    #         for item in range(self.k):
-    #             neighbors.append(distances[item][1])
-    #
-    #         # Making predictions
-    #         output_pred.append(st.mode(a=neighbors, axis=None, keepdims=False).mode)
-    #
-    #     return np.array(output_pred)
+    def predict(self, X_test: np.ndarray) -> np.ndarray:
+        output_pred = []
+
+        # Calculating distances
+        for i in range(X_test.shape[0]):  # for each sample in X_test
+            distances = []  # To store the distances
+            for j in range(self.X_train.shape[0]):  # for each sample in X_train
+                if self.metric == "euclidean":
+                    dist = euclidean_distances(self.X_train[j, :], X_test[i, :])  # Euclidean distance
+                else:
+                    dist = manhattan_distances(self.X_train[j, :], X_test[i, :])  # Manhattan distance
+                distances.append((dist, self.y_train[j]))
+            distances = sorted(distances)  # sorting distances in ascending order, # Order each neighbor
+
+            # Getting k nearest neighbors
+            neighbors = []
+            for item in range(self.k):
+                neighbors.append(distances[item][1])
+
+            # Making predictions
+            output_pred.append(st.mode(a=neighbors, axis=None, keepdims=False).mode)
+
+        return np.array(output_pred)
 
     def predict(self, X_data: np.ndarray) -> np.ndarray:
         # Order each neighbor
