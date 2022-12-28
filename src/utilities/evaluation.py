b8fb41afe49e1dbdbea0539e3539d7bf8ba44e6f
RuieX <runjiexia28@gmail.com>
2022-12-28 14:00:07 +0100

    fianl check

--- src/utilities/evaluation.py
+++ src/utilities/evaluation.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 import joblib
 import pandas as pd
+import seaborn as sns
 
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 
@@ -58,3 +59,61 @@ class EvaluatedModel:
         :return:
         """
         return joblib.load(f'../models_evaluation/{model_name}.pkl')
+
+
+class ModelsComparison:
+    def __init__(self):
+        # load all the final models
+        b_nb_model = EvaluatedModel.load_evaluation("beta_naivebayes")
+        knn_model = EvaluatedModel.load_evaluation("knearestneighbors")
+        rf_model = EvaluatedModel.load_evaluation("randomforest")
+        svc_lin_model = EvaluatedModel.load_evaluation("svclinear")
+        svc_pol_model = EvaluatedModel.load_evaluation("svcpolynomial")
+        svc_rbf_model = EvaluatedModel.load_evaluation("svcrbf")
+        self.models = [svc_lin_model, svc_pol_model, svc_rbf_model, rf_model, knn_model, b_nb_model]
+        self.all_performances = None
+
+    def rank_model(self):
+        """
+        ranking based on accuracy score of the model
+        :return:
+        """
+        min_acc_score = None
+        acc_score = None
+
+        for i, m in enumerate(self.models):
+            if i == 0:
+                min_acc_score = m
+                acc_score = m.test_eval.acc_score
+            if acc_score < m.test_eval.acc_score:
+                min_acc_score = m
+                acc_score = m.test_eval.acc_score
+
+        return min_acc_score.model, acc_score
+
+    def get_performance_df(self):
+        """
+        return a pd.DataFrame of the models and their accuracy score
+        :return:
+        """
+        perf_records = []
+        for model in self.models:
+            record = {
+                "model": model.model_name,
+                "Accuracy Score": model.test_eval.acc_score}
+            perf_records.append(record)
+
+        self.all_performances = pd.DataFrame.from_records(data=perf_records)
+
+        return self.all_performances
+
+    def performance_plot(self):
+        """
+        plot the score
+        :return:
+        """
+        sns.set(rc={"figure.figsize":(8, 4)})
+        plot = sns.lineplot(data=self.all_performances, x="model", y="Accuracy Score", hue="model",style="model", palette="bright", markers=True)
+        plot.tick_params(axis="x", rotation=90)
+
+        return plot
